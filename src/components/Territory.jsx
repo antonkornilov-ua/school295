3d05bb2bd7cbaa2a101e5f7513414c209dc209e2
antonkornilov-ua <83725472+antonkornilov-ua@users.noreply.github.com>
2023-02-28 18:19:35 +0200

    bugs

--- src/components/Territory.jsx
+++ src/components/Territory.jsx
@@ -4,7 +4,7 @@ import { territoryArray } from '../helpers/territoryArray';
 const Territory = () => {
     const gridStyle = `grid lg:grid-cols-2 grid-cols-1 gap-x-10 mt-10`;
     const rightColStyle = `lg:flex items-center justify-between lg:text-6xl font-bold text-gray-700 text-center text-2xl lg:mb-0 mb-5`;
-    const buttonStyle = ` mx-auto text-white/90 text-center bg-gray-700 px-4 py-2 rounded-xl  shadow-xl`;
+    const buttonStyle = ` mx-auto text-white/90 text-center bg-gray-700 px-4 py-2 mx-5 rounded-xl shadow-xl`;
 
     return (
         <div className={gridStyle}>
